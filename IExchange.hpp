588005daaa3596dbdcd3b2a284fcf5f1a79c9705
Benjamin Schwartz <benjaminschwartz@mac.lan>
2024-11-19 22:20:05 +1100

    Completed implementation and added some tests

--- IExchange.hpp
+++ IExchange.hpp
@@ -15,21 +15,21 @@ class IExchange {
     // Add an order to the exchange
     // @return unique identifier for the order or descriptive error on failure
     virtual std::expected<std::uint64_t, std::string> AddOrder(
-        const std::string& instrument, Side side, std::int64_t price,
+        const std::string &instrument, Side side, std::int64_t price,
         std::uint32_t quantity) = 0;
 
     // Remove an order from the exchange
     // @return success or failure
-    virtual bool RemoveOrder(const std::string& instrument, Side side,
+    virtual bool RemoveOrder(const std::string &instrument, Side side,
                              std::uint64_t orderId) = 0;
 
     // callback to indicate an order has been matched
-    std::function<void(const std::string& instrument, std::uint64_t orderId,
+    std::function<void(const std::string &instrument, std::uint64_t orderId,
                        std::int64_t tradedPrice, std::uint32_t tradedQuantity)>
         OrderTraded{};
 
     // callback to indicate the best price has changed for a given instrument
-    std::function<void(const std::string& instrument, std::int64_t bidPrice,
+    std::function<void(const std::string &instrument, std::int64_t bidPrice,
                        std::uint32_t bidTotalQuantity, std::int64_t askPrice,
                        std::uint32_t askTotalQuantity)>
         BestPriceChanged{};
--- exchange.cpp
+++ exchange.cpp
@@ -24,112 +24,220 @@ Assumptions:
     * Order only trades if it trades completely (sufficient quantity)
  */
 
+#include <cassert>
 #include <deque>
+#include <functional>
+#include <iostream>
 #include <map>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
 
 #include "IExchange.hpp"
 
-class Exchange : IExchange {
+class Exchange : public IExchange {
    public:
-    ~Exchange() override;
+    Exchange() {}
+
+    ~Exchange() override = default;
 
     std::expected<std::uint64_t, std::string> AddOrder(
-        const std::string& instrument, Side side, std::int64_t price,
-        std::uint32_t quantity) override
-    {
-        if (price <= 0)
-            return std::unexpected("Price must be positive");
-        else if (quantity <= 0)
-            return std::unexpected("Quantity must be positive");
+        const std::string &instrument, Side side, std::int64_t price,
+        std::uint32_t quantity) override;
 
-        if (side == Side::BUY) {
-            auto it = m_bidMap.find(instrument);
+    bool RemoveOrder(const std::string &instrument, Side side,
+                     std::uint64_t orderId) override;
 
-            // NEW INSTRUMENT
-            if (it == m_bidMap.end()) {
-                m_bidMap[instrument][price].first.push_back(
-                    Order{m_bidOrderCount, price, quantity});
-                m_askMap[instrument];  // insert new entry on ask side
-            }
-            else {
-                auto price_iter = m_askMap[instrument].begin();
-                // Can trade at a given price
-                while (price_iter != m_askMap[instrument].end() &&
-                       price >= price_iter->first) {
-                    auto order_iter = price_iter->second.first.begin();
-                    while (order_iter != price_iter->second.first.end() &&
-                           quantity >= order_iter->quantity) {
-                        OrderTraded(instrument, order_iter->orderId,
-                                    price_iter->first, order_iter->quantity);
-                        quantity -= order_iter->quantity;
-                        // Decrement total quantity for that price
-                        price_iter->second.second -= order_iter->quantity;
-                        order_iter = price_iter->second.first.erase(order_iter);
-                    }
-                    // We have cleared a price-level
-                    if (order_iter == price_iter->second.first.end()) {
-                        m_askMap[instrument].erase(price_iter);
-                        BestPriceChanged(instrument,
-                                         m_bidMap[instrument].begin()->first, 
-                                         m_bidMap[instrument].begin()->second.second,
-                                         m_askMap[instrument].begin()->first,
-                                         m_askMap[instrument].begin()->second.second
-                                         );
-                    }
-                }
-            }
-            return m_bidOrderCount++;
-        }
-        else {
-            auto it = m_askMap.find(instrument);
-
-            // NEW INSTRUMENT
-            if (it == m_askMap.end()) {
-                m_askMap[instrument][price].first.push_back(
-                    Order{m_askOrderCount, price, quantity});
-                m_bidMap[instrument];  // insert new entry on bid side
-            }
-            else {
-            }
-            return m_askOrderCount++;
-        }
-    };
-
-    bool RemoveOrder(const std::string& instrument, Side side,
-                     std::uint64_t orderId) override
-    {
-        return 0;
-    }
+    void printInstrumentBooks(const std::string &instrument);
 
    private:
     struct Order {
         std::uint64_t orderId;
-        std::int64_t price; /* Might not need this */
         std::uint32_t quantity;
     };
 
     using PriceT = uint64_t;
 
-    // Prices sorted in descending order
-    using BidPriceTimeT = std::map<
-        PriceT,
-        std::pair<std::deque<Order>, uint32_t /* best price total quantity */>,
-        std::greater<>>;
+    template <typename Func>
+    using PriceLevelT = std::map<PriceT, std::deque<Order>, Func>;
 
-    // Prices sorted in ascending order
-    using AskPriceTimeT =
-        std::map<PriceT, std::pair<std::deque<Order>,
-                                   uint32_t /* best price total quantity */>>;
+    template <typename Func>
+    using OrderMapT =
+        std::unordered_map<std::string /* instrument */, PriceLevelT<Func>>;
 
-    std::unordered_map<std::string /* instrument */, BidPriceTimeT> m_bidMap;
-    std::unordered_map<std::string /* instrument */, AskPriceTimeT> m_askMap;
+    OrderMapT<std::less<PriceT>> m_askMap;
+    OrderMapT<std::greater<PriceT>> m_bidMap;
 
-    std::unordered_set<std::uint64_t> m_bidOrderIds;
-    std::unordered_set<std::uint64_t> m_askOrderIds;
+    std::unordered_map<uint64_t /* orderId */, PriceT> m_bidPriceMap;
+    std::unordered_map<uint64_t /* orderId */, PriceT> m_askPriceMap;
 
+    // For generating unique order IDs
     uint64_t m_bidOrderCount = 0;
     uint64_t m_askOrderCount = 0;
 };
+
+std::expected<std::uint64_t, std::string> Exchange::AddOrder(
+    const std::string &instrument, Side side, std::int64_t price,
+    std::uint32_t quantity)
+{
+    bool best_price_changed = false;
+    auto process_orders = [&](auto &targetMap, auto &oppositeMap,
+                              auto &&priceMatch) {
+        auto price_iter = oppositeMap[instrument].begin();
+        while (price_iter != oppositeMap[instrument].end() &&
+               priceMatch(price_iter->first, price)) {
+            auto &orders = price_iter->second;
+            auto order_iter = orders.begin();
+            while (order_iter != orders.end() && quantity > 0) {
+                // Full trade
+                if (order_iter->quantity <= quantity) {
+                    OrderTraded(instrument, order_iter->orderId,
+                                price_iter->first, order_iter->quantity);
+                    quantity -= order_iter->quantity;
+                    order_iter = orders.erase(order_iter);
+                }
+                // Partial trade
+                else {
+                    OrderTraded(instrument, order_iter->orderId,
+                                price_iter->first, quantity);
+                    order_iter->quantity -= quantity;
+                    quantity = 0;
+                }
+            }
+            if (orders.empty()) {
+                // Remove price level if all orders are processed
+                best_price_changed = true;
+                price_iter = oppositeMap[instrument].erase(price_iter);
+            }
+            else {
+                ++price_iter;
+            }
+        }
+        if (quantity > 0) {
+            if (side == Side::BUY) {
+                m_bidPriceMap[price] = m_bidOrderCount;
+            }
+            else {
+                m_askPriceMap[price] = m_bidOrderCount;
+            }
+            targetMap[instrument][price].push_back(
+                Order{side == Side::BUY ? m_bidOrderCount++ : m_askOrderCount++,
+                      quantity});
+        }
+    };
+
+    if (side == Side::BUY) {
+        process_orders(m_bidMap, m_askMap, std::less_equal<PriceT>{});
+    }
+    else {
+        process_orders(m_askMap, m_bidMap, std::greater_equal<PriceT>{});
+    }
+    return 0;
+}
+
+bool Exchange::RemoveOrder(const std::string &instrument, Side side,
+                           std::uint64_t orderId)
+{
+    auto remove_order = [&](auto &targetMap, auto &targetPriceMap) {
+        if (!targetMap.count(instrument) || !targetPriceMap.count(orderId)) {
+            return false;
+        }
+        PriceT price = targetPriceMap[orderId];
+        auto &price_level = targetMap[instrument][price];
+        auto order_iter = price_level.begin();
+        while (order_iter != price_level.end()) {
+            if (order_iter->orderId == orderId) {
+                price_level.erase(order_iter);
+                return true;
+            }
+        }
+        return false;
+    };
+
+    if (side == Side::BUY) {
+        return remove_order(m_bidMap, m_bidPriceMap);
+    }
+    else {
+        return remove_order(m_askMap, m_askPriceMap);
+    }
+}
+
+void Exchange::printInstrumentBooks(const std::string &instrument)
+{
+    std::cout << "Instrument=" << instrument << std::endl;
+    auto printPriceLevel = [&](auto &targetMap) {
+        if (!targetMap.count(instrument)) {
+            return;
+        }
+        const auto &price_level = targetMap[instrument];
+        for (const auto &[key, val] : price_level) {
+            std::cout << "Price=" << key << "\n";
+            for (const auto &order : val) {
+                std::cout << "{id=" << order.orderId
+                          << ", quantity=" << order.quantity << "} ";
+            }
+            std::cout << "\n";
+        }
+    };
+    std::cout << "-----------------------------\n";
+    std::cout << "Bids:\n";
+    printPriceLevel(m_bidMap);
+    std::cout << "-----------------------------\n";
+    std::cout << "Asks:\n";
+    printPriceLevel(m_askMap);
+}
+
+int main()
+{
+    Exchange ex;
+    auto orderTraded = [](const std::string &instrument, std::int64_t orderId,
+                          std::uint64_t tradedPrice,
+                          std::int64_t tradedQuantity) {
+        std::cout << "Order Traded: Instrument=" << instrument
+                  << ", OrderId=" << orderId << ", TradedPrice=" << tradedPrice
+                  << ", Traded Quantity=" << tradedQuantity << "\n";
+    };
+    auto bestPriceChanged =
+        [](const std::string &instrument, std::int64_t bidPrice,
+           std::uint32_t bidTotalQuantity, std::int64_t askPrice,
+           std::int32_t askTotalQuantity) {
+            std::cout << "Order Traded: Instrument=" << instrument
+                      << ", BidPrice=" << bidPrice
+                      << ", BidTotalQuantity=" << bidTotalQuantity
+                      << ", AskPrice=" << askPrice
+                      << ", AskTotalQuantity=" << askTotalQuantity << "\n";
+        };
+    ex.OrderTraded = orderTraded;
+    ex.BestPriceChanged = bestPriceChanged;
+
+    // Add some resting orders
+    assert(ex.AddOrder("AAPL", Side::BUY, 69, 1000));
+    assert(ex.AddOrder("AAPL", Side::SELL, 75, 750));
+    assert(ex.AddOrder("AAPL", Side::BUY, 70, 1000));
+    assert(ex.AddOrder("AAPL", Side::SELL, 76, 750));
+    assert(ex.AddOrder("AAPL", Side::BUY, 68, 1000));
+    assert(ex.AddOrder("AAPL", Side::SELL, 73, 750));
+
+    // Partial order executes
+    assert(ex.AddOrder("AAPL", Side::SELL, 70, 750));
+
+    // Level clears
+    assert(ex.AddOrder("AAPL", Side::SELL, 70, 750));
+
+    // Clear a level, then partially another level
+    assert(ex.AddOrder("AAPL", Side::BUY, 73, 1000));
+
+    // Add on existing levels
+    assert(ex.AddOrder("AAPL", Side::BUY, 69, 500));
+    assert(ex.AddOrder("AAPL", Side::BUY, 69, 1000));
+    assert(ex.AddOrder("AAPL", Side::BUY, 68, 1000));
+
+    // Clear through two and a bit orders on level
+    assert(ex.AddOrder("AAPL", Side::SELL, 69, 1750));
+
+    assert(ex.AddOrder("AAPL", Side::BUY, 69, 1000));
+
+    ex.printInstrumentBooks("AAPL");
+    std::cout << "\n";
+
+    return 0;
+}
